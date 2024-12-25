4ef6c432bbc2e95844a28c29d6eee94939a7f82c
ToddLiao <toddliao.dev@gmail.com>
2024-12-26 00:22:23 +0800

    feat: add flyway

--- src/main/kotlin/com/example/config/DatabaseConfig.kt
+++ src/main/kotlin/com/example/config/DatabaseConfig.kt
@@ -1,19 +1,27 @@
 package com.example.plugins
 
-import io.github.cdimascio.dotenv.Dotenv
 import org.jetbrains.exposed.sql.Database
+import org.flywaydb.core.Flyway
 
 object DatabaseConfig {
     fun connect() {
-        val dotenv = Dotenv.load()
-        val databaseUrl = dotenv["DATABASE_URL"] ?: throw IllegalStateException("DATABASE_URL is not set")
-        val user = dotenv["DATABASE_USER"] ?: throw IllegalStateException("DATABASE_USER is not set")
-        val password = dotenv["DATABASE_PASSWORD"] ?: throw IllegalStateException("DATABASE_PASSWORD is not set")
+
+        val databaseUrl = System.getenv("DATABASE_URL") ?: throw IllegalStateException("DATABASE_URL is not set")
+        val user = System.getenv("DATABASE_USER") ?: throw IllegalStateException("DATABASE_USER is not set")
+        val password = System.getenv("DATABASE_PASSWORD") ?: throw IllegalStateException("DATABASE_PASSWORD is not set")
         Database.connect(
             url = databaseUrl,
             driver = "org.postgresql.Driver",
             user = user,
             password = password,
         )
+
+
+        val flyway = Flyway.configure()
+            .dataSource(databaseUrl, user, password)
+            .load()
+
+        flyway.migrate()
+
     }
 }
